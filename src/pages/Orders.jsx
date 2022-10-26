09b15fc3db286c6dfd94ad1454f6391c23edc80c
Yue Zhang <architectyuezhang@gmail.com>
2022-10-26 14:22:36 -0400

    finish order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -13,19 +13,36 @@ import {
   Edit,
   Inject,
 } from "@syncfusion/ej2-react-grids";
-import { orderData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { contextMenuItems, ordersGrid, ordersData } from "../data/dummy";
 import { Header } from "../components";
 
 const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridComp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, idx) => (
             <ColumnDirective key={idx} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
