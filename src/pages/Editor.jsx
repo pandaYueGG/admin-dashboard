990d2713d6edc56d2ebe1b92ead9e44c2121d7ae
Yue Zhang <architectyuezhang@gmail.com>
2022-10-26 16:44:41 -0400

    finish Editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className="m-2 md:m-10 mt-20 p-2 md:p-10 bg-white rounded-2xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
