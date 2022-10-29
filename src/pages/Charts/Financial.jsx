13cfc8998ea626f05df75acd6f90c0b2b0185d50
Yue Zhang <architectyuezhang@gmail.com>
2022-10-28 23:45:07 -0400

    keep working on financial chart

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from "@syncfusion/ej2-react-charts";
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
+
+const startDate = new Date("2000, 1, 1");
+
+const valueFilter = (value) => {
+  if (value.x >= startDate) {
+    return value.x, value.high, value.low;
+  }
+};
+
+const filteredValues = financialChartData.filter(valueFilter);
 
 const Financial = () => {
-  return <div>Financial</div>;
+  const { currentColor, currentMode } = useStateContext();
+  return (
+    <div>
+      <Header category="Financial" title="Financial Summary" />
+      <div className="w-full"></div>
+    </div>
+  );
 };
 
 export default Financial;
