5df6ebfaf2df9ffa1fa2c5ecb77be5eaa53a2767
Yue Zhang <architectyuezhang@gmail.com>
2022-10-28 23:19:32 -0400

    pie chart works

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationDataLabel,
+  Inject,
+  AccumulationTooltip,
+} from "@syncfusion/ej2-react-charts";
+import { useStateContext } from "../../contexts/ContextProvider";
 
-const PieChart = () => {
-  return <div>Piechart</div>;
+const PieChart = ({ id, data, legendVisibility, height }) => {
+  const { currentColor, currentMode } = useStateContext();
+  return (
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{
+        visible: legendVisibility,
+        background: currentColor,
+        opacity: 0.3,
+      }}
+      background={currentMode === "Dark" ? "#303236" : "#fff"}
+      tooltip={{ enable: true }}
+    >
+      <Inject
+        services={[
+          AccumulationLegend,
+          PieSeries,
+          AccumulationDataLabel,
+          AccumulationTooltip,
+        ]}
+      />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="80%"
+          explode
+          explodeOffset="20%"
+          dataLabel={{
+            visible: true,
+            name: "text",
+            position: "Inside",
+            font: {
+              fontWeight: "600",
+              color: "white",
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
 };
 
 export default PieChart;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { pieChartData } from "../../data/dummy";
+import { Header, PieChart } from "../../components";
 
 const Pie = () => {
-  return <div>Pie</div>;
+  return (
+    <div>
+      <Header category="Pie" title="Bussiness Cost Breakdown" />
+      <div className="w-full">
+        <PieChart
+          id="chart-pie"
+          data={pieChartData}
+          legendVisibility
+          height="full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Pie;
