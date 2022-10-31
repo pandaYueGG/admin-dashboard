b600df22ff24973ee478875999c4d79ebee9b612
Yue Zhang <architectyuezhang@gmail.com>
2022-10-31 17:52:41 -0400

    working on pyramid chart

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  AccumulationDataLabel,
+  AccumulationTooltip,
+  AccumulationSelection,
+  PyramidSeries,
+  Inject,
+} from "@syncfusion/ej2-react-charts";
+import { PyramidData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Pyramid = () => {
-  return <div>Pyramid</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div>
+      <Header category="Pyramid" title="Foodie Pyramid" />
+    </div>
+  );
 };
 
 export default Pyramid;
